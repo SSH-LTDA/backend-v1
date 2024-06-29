8696e03f836a8c0e7e4f2046905125594e86a671
ismaelsilvat <ismaelteixeiradas1@hotmail.com>
2024-06-29 15:39:53 -0300

    feat: Developing login.dto.ts

---
+++ src/api/auth/dtos/login.dto.ts
@@ -0,0 +1,10 @@
+import z from "zod";
+
+export const LoginSchema
+ = z.object({
+	email: z.string().email(),
+	password: z.string().min(6),
+});
+
+
+export type LoginDto = z.infer<typeof LoginSchema>;
