1dc75479483172daeed9b5c5f2e618466318756d
ismaelsilvat <ismaelteixeiradas1@hotmail.com>
2024-06-29 15:40:10 -0300

    feat: Adding authRoutes to routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,14 @@
-import express from 'express'
-import {userRoutes} from "./api/users/user.routes";
-import {accommodationRoutes} from "./api/accomodation/accommodation.routes";
-import {bookingRoutes} from "./api/booking/booking.routes";
+import express from "express";
+import { userRoutes } from "./api/users/user.routes";
+import { accommodationRoutes } from "./api/accomodation/accommodation.routes";
+import { bookingRoutes } from "./api/booking/booking.routes";
+import authRoutes from "./api/auth/auth.routes";
 
-const router = express.Router()
+const router = express.Router();
 
-router.use('/users', userRoutes)
-router.use('/accommodations', accommodationRoutes)
-router.use('/bookings', bookingRoutes)
+router.use("/users", userRoutes);
+router.use("/accommodations", accommodationRoutes);
+router.use("/bookings", bookingRoutes);
+router.use(authRoutes);
 
-export { router }
+export { router };
