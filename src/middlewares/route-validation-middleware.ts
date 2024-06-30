e481818441f9d85ab40fc65f622135cf02c94770
ismaelsilvat <ismaelteixeiradas1@hotmail.com>
2024-06-30 17:30:15 -0300

    refactor: Formatting with prettier

--- src/middlewares/route-validation-middleware.ts
+++ src/middlewares/route-validation-middleware.ts
@@ -1,8 +1,8 @@
-import { Request, Response, NextFunction } from 'express';
-import { z, ZodError } from 'zod';
+import { Request, Response, NextFunction } from "express";
+import { z, ZodError } from "zod";
 
-import {BadRequestError} from "../errors/bad-request.error";
-import {GeneralError} from "../errors/general.error";
+import { BadRequestError } from "../errors/bad-request.error";
+import { GeneralError } from "../errors/general.error";
 
 export const routeValidationMiddleware = (schema: z.ZodObject<any, any>) => {
 	return (req: Request, res: Response, next: NextFunction) => {
@@ -12,12 +12,12 @@ export const routeValidationMiddleware = (schema: z.ZodObject<any, any>) => {
 		} catch (error) {
 			if (error instanceof ZodError) {
 				const errorMessages = error.errors.map((issue: any) => ({
-					message: `${issue.path.join('.')} is ${issue.message}`,
-				}))
-				throw new BadRequestError("invalid_data", errorMessages)
+					message: `${issue.path.join(".")} is ${issue.message}`,
+				}));
+				throw new BadRequestError("invalid_data", errorMessages);
 			} else {
-				throw new GeneralError("internal_error", "Internal Server Error")
+				throw new GeneralError("internal_error", "Internal Server Error");
 			}
 		}
 	};
-}
+};
