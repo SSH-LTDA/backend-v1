179a82db1e0f20593a548ee560ee32dc67bc5860
ismaelsilvat <ismaelteixeiradas1@hotmail.com>
2024-06-30 17:30:26 -0300

    feat: Importing stripe routes at routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { userRoutes } from "./api/users/user.routes";
 import { accommodationRoutes } from "./api/accomodation/accommodation.routes";
 import { bookingRoutes } from "./api/booking/booking.routes";
 import authRoutes from "./api/auth/auth.routes";
+import stripeRoutes from "./api/stripe/stripe.routes";
 
 const router = express.Router();
 
@@ -10,5 +11,6 @@ router.use("/users", userRoutes);
 router.use("/accommodations", accommodationRoutes);
 router.use("/bookings", bookingRoutes);
 router.use(authRoutes);
+router.use(stripeRoutes);
 
 export { router };
